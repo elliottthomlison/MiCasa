ac8a500c0e4c72cabbc11d82532ed11f77bfb37b
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-18 16:45:50 -0700

    Add stylistic changes to the search bar

--- react-front-end/src/components/BackgroundHeader.js
+++ react-front-end/src/components/BackgroundHeader.js
@@ -5,9 +5,9 @@ function BackgroundHeader() {
   return (
     <div class="bbbootstrap">
       <div class="container">
-  	    <h1>Let's find UrCasa so that you can say MiCasa</h1>
-		    <div class="search-box">
-			    <input type="text" class="search-box-input" placeholder="What are you looking for ?"/>
+  	<h1 id="header" >Let's find UrCasa so that you can say <em>MiCasa!</em></h1>
+		<div class="search-box">
+			<input type="text" class="search-box-input" placeholder="What are you looking for ?"/>
 			    <button class="search-box-btn">
           <a href='./map' class='myButton'><font size="20px" color="#FF7A59"><button>SEARCH</button></font></a></button>
 		    </div>     
