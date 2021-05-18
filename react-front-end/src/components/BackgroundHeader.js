83237db8e44e70c02d33f9bea36aafb0cd280671
elliottthomlison <71367849+elliottthomlison@users.noreply.github.com>
2021-05-18 15:57:51 -0700

    Change the header search bar to allow text

--- react-front-end/src/components/BackgroundHeader.js
+++ react-front-end/src/components/BackgroundHeader.js
@@ -4,16 +4,14 @@ import './BackgroundHeader.css'
 function BackgroundHeader() {
   return (
     <div class="bbbootstrap">
-  <div class="container">
-  	<h1>ANIMATED SEARCH BOX</h1>
-		<div class="search-box">
-			<input type="text" class="search-box-input" placeholder="What are you looking for ?"/>
-			<button class="search-box-btn">
-				<i class="search-box-icon material-icons">search</i>
-			</button>
-		</div>
-  </div>
-
+      <div class="container">
+  	    <h1>Let's find UrCasa so that you can say MiCasa</h1>
+		    <div class="search-box">
+			    <input type="text" class="search-box-input" placeholder="What are you looking for ?"/>
+			    <button class="search-box-btn">
+          <a href='./map' class='myButton'><font size="20px" color="#FF7A59"><button>SEARCH</button></font></a>			</button>
+		    </div>     
+      </div>
   </div>
   )
 }
