0851a487da6891679108f7a51a80524e5aea40a7
ban-mazin <ban.qaddoori89@gmail.com>
2021-05-19 15:39:26 +0000

    update database

--- express-back-end/db/seeds/2_property.sql
+++ express-back-end/db/seeds/2_property.sql
@@ -3,20 +3,20 @@ values (1, 1, '7 Fairfield Street', 'https://github.com/elliottthomlison/MiCasa/
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
 values (2, 2, 'W 43rd Ave', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house2.jpg?raw=true', 'Nelson Street',  49.232681,  -123.131161, '$900 permonth', '2 bedrooms', '1 bathroom', '900 SQF', 'no pets', 'unfurnished', 'laundry', '2 parking spots');
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
-values (3, 3, 'Oak st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house3.jpg?raw=true', 'Vancouver Bc', 49.230927, -123.13292,'$600 permonth', '1 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'unfurnished', 'no laundry', 'no parking spots');
+values (3, 3, 'Oak st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house3.jpg?raw=true', 'Vancouver Bc', 49.230927, -123.13292,'$600 permonth', '1 bedroom', '1 bathroom', '1000 SQF', ' pets ok', 'unfurnished', 'no laundry', 'no parking spots');
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
 values (4, 4, 'Mantgomery St', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house5.jpg?raw=true', 'Vancouver', 49.227833,  -123.130774, '$700 permonth', '2 bedrooms', '1 bathroom', '900 SQF', 'no pets', 'furnished', '1 laundry', '2 parking spots');
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
-values (5, 5, 'Tisdall st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house7.jpg?raw=true', 'Vancouver',  49.227973, -123.122106,'$500 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'furnished', '1 laundry', '2 parking spots');
+values (5, 5, 'Tisdall st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house7.jpg?raw=true', 'Vancouver',  49.233461, -123.138353,'$500 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'furnished', '1 laundry', '2 parking spots');
 
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
-values (6, 6, 'W 50th Ave', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house6.jpg?raw=true', 'Vancouver',  49.225703, -123.121848, '$700 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'unfurnished', '1 laundry', '2 parking spots');
+values (6, 6, 'W 50th Ave', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house6.jpg?raw=true', 'Vancouver',  49.228949, -123.13801, '$700 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'unfurnished', '1 laundry', '2 parking spots');
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
 values (7, 7, 'Granville Street', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house12.jpg?raw=true', 'Vancouver', 49.229739, -123.135967, '$600 permonth', '1 bedroom', '1 bathroom', '900 SQF', ' pets ok', 'furnished', 'no laundry', 'no parking spots');
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
-values (8, 8, 'Columbia st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house10.jpg?raw=true', 'Vancouver', 49.226207, -123.111763,'$500 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'unfurnished', '1 laundry', 'no parking spots');
+values (8, 8, 'Columbia st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house10.jpg?raw=true', 'Vancouver', 49.230546,  -123.127152,'$500 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'unfurnished', '1 laundry', 'no parking spots');
 insert into property (id, user_id, address, img_url, city, latitude, longitude, minimum_price, bedroomNo, bathroomNo, size, pets, furnished, laundry, parking) 
-values (9, 9, 'Chester st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house4.jpg?raw=true', 'Vancouver',  49.228315, -123.086014,'$500 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'furnished', '1 laundry', '2 parking spots');
+values (9, 9, 'Chester st', 'https://github.com/elliottthomlison/MiCasa/blob/bans_branch/images/house4.jpg?raw=true', 'Vancouver',  49.231863, -123.135564,'$500 permonth', '2 bedrooms', '1 bathroom', '1000 SQF', 'no pets', 'furnished', '1 laundry', '2 parking spots');
 
 
 
